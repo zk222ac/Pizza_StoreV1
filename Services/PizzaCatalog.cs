77202354062950d3a410e99f0d68da91de98cc5d
Zuhair Haroon Khan <zk222ac@gmail.com>
2021-02-11 22:18:18 +0100

    Add project files.

---
+++ Services/PizzaCatalog.cs
@@ -0,0 +1,28 @@
+﻿using Pizza_StoreV1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pizza_StoreV1.Services
+{
+    public class PizzaCatalog
+    {
+        private Dictionary<int, Pizza> pizzas { get; }
+        public PizzaCatalog()
+        {
+            pizzas = new Dictionary<int, Pizza>();
+            pizzas.Add(1, new Pizza() { Id = 1, Name = "Cheese_pizza", Description = " Made of cheese", Price = 98, ImageName = "Cheese_pizza.jfif" });
+            pizzas.Add(2, new Pizza() { Id = 2, Name = "Bufalla_pizza", Description = " Made of bufalla", Price = 59, ImageName = "Bufalla_pizza.jfif" });
+            pizzas.Add(3, new Pizza() { Id = 3, Name = "Chicken_pizza", Description = " Made of chicken", Price = 120, ImageName = "Chicken_pizza.jfif" });
+            pizzas.Add(4, new Pizza() { Id = 4, Name = "Mozzarella_pizza", Description = " Made of mozzarella", Price = 77, ImageName = "Mozzarella_pizza.jfif" });
+            pizzas.Add(5, new Pizza() { Id = 5, Name = "Vegetable_pizza", Description = " Made of vegetars", Price = 88, ImageName = "Vegetable_pizza.jfif" });
+        }
+
+        public Dictionary<int, Pizza> AllPizzas()
+        {
+            // not implemented yet , We return an empty dictionary to avoid compile error
+            return new Dictionary<int, Pizza>();
+        }
+    }
+}
